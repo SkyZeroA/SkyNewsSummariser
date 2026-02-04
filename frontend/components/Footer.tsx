4df63e479ea56f5d9f8c90a287ce8b728f68dca4
Salahuddin, Adnan (Software Engineering Graduate - Sky) <adnan.salah84@gmail.com>
2026-02-04 10:00:57 +0000

    Added Header and Footer Component

---
+++ frontend/components/Footer.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function Footer() {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer className="w-full border-t border-zinc-200 bg-white dark:border-zinc-800 dark:bg-black">
+  
+    <p>this is footer</p>
+
+    </footer>
+  );
+}
+
---
+++ frontend/components/Header.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="w-full border-b border-zinc-200 bg-white dark:border-zinc-800 dark:bg-black">
+      <p> This is header</p>
+    </header>
+  );
+}
+
