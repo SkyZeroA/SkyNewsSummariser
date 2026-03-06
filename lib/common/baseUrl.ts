9a916395045f0a2b1455e68c5769d4957cc3a106
Harry Taylor <harry.taylor3@sky.uk>
2026-03-06 09:42:21 +0000

    publishSummary

--- lib/common/baseUrl.ts
+++ lib/common/baseUrl.ts
@@ -4,7 +4,6 @@ export const getApiBaseUrl = (event: APIGatewayProxyEvent): string => {
 	const headers = event.headers || {};
 	const rawProto = headers['x-forwarded-proto'] || headers['X-Forwarded-Proto'];
 	const proto = (Array.isArray(rawProto) ? rawProto[0] : rawProto || 'https').split(',')[0].trim() || 'https';
-	const domain = event.requestContext.domainName;
-	const [stage] = event.requestContext.stage;
+	const { domainName: domain, stage } = event.requestContext;
 	return `${proto}://${domain}/${stage}`;
 };
